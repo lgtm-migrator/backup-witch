d589d84cc1e52e9314a9b76f9588ea5da9ace0c4
Alexander Ivanov <76620843+ark-leaf@users.noreply.github.com>
2022-03-21 19:48:26 +0200

    feat: add utility script for generating systemd service file

---
+++ systemd-init.sh
@@ -0,0 +1,28 @@
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+ExecStart="python3 ${SCRIPT_DIR}/src/main.py"
+
+read -r -d '' SERVICE << EOM
+[Unit]
+Description=backup-witch
+After=network-online.target
+Wants=network-online.target systemd-networkd-wait-online.service
+
+StartLimitIntervalSec=600
+StartLimitBurst=5
+
+[Service]
+Type=exec
+Environment=PYTHONUNBUFFERED=1
+
+Restart=on-failure
+RestartSec=60s
+
+ExecStart=${ExecStart}
+
+[Install]
+WantedBy=default.target
+
+EOM
+
+mkdir -p ~/.config/systemd/user/ && printf "%s" "$SERVICE" > "${_}/backup-witch.service"
