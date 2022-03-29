45d4411a7333a18dc3e591e4127c32a9a38129f4
Alexander Ivanov <76620843+ark-leaf@users.noreply.github.com>
2022-03-29 14:26:26 +0300

    docs: update sample configs

--- sample_configs/cmd_args.py
+++ sample_configs/cmd_args.py
@@ -1,14 +1,10 @@
-LIST_APPS_AND_COPY_FILES_INTERVAL = 1800  # seconds: 30 minutes
-
-SYNC_DELETIONS_INTERVAL = 43200  # seconds: 12 hours
+LIST_APPS_AND_COPY_FILES_INTERVAL = 900  # seconds: 15 minutes
 
 NO_TRAVERSE_MAX_AGE = 86400  # seconds: 24 hours
 
-RCLONE_FLAGS = '--fast-list --buffer-size 16M --use-mmap --s3-no-check-bucket --stats 1m --log-level INFO ' \
-               '--stats-file-name-length 0 --links'
+RCLONE_FLAGS = '--fast-list --drive-chunk-size 64M --transfers 10 --buffer-size 16M --use-mmap ' \
+               '--s3-no-check-bucket --stats 1m --log-level INFO --stats-file-name-length 0'
 
-APPS_LIST_FILE_NAME = '.list_of_installed_apps.txt'
-
-BACKUP_WITCH_REMOTE = 'CHANGE_ME-!'  # set to appropriate rclone remote, e.g. 'remote:'
+IGNORE_PERMISSION_ERRORS = True
 
 DEBUG = False
