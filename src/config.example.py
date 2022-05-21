3f242aa6f0a67e649e4e90816716f6c69d608176
Alexander Ivanov <76620843+ark-key@users.noreply.github.com>
2022-05-21 23:55:33 +0300

    refactor: update config.example.py

--- src/config.example.py
+++ src/config.example.py
@@ -6,4 +6,17 @@ CONFIG = Configuration(
     BACKUP_DESTINATION_LATEST='example-remote:@backup-witch/latest',
 
     BACKUP_DESTINATION_PREVIOUS='example-remote:@backup-witch/previous',
+
+    RCLONE_FLAGS=[
+        '--fast-list',
+        '--drive-chunk-size 64M',
+        '--transfers 10',
+        '--buffer-size 16M',
+        '--use-mmap',
+        '--s3-no-check-bucket',
+        '--stats 1m',
+        '--log-level INFO',
+        '--stats-file-name-length 0',
+        '--retries 1',
+    ]
 )
