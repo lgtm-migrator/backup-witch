e7bc4e058c0f48eb921facc6a8027883c9654d17
Alexander Ivanov <76620843+ark-key@users.noreply.github.com>
2022-05-27 01:04:58 +0300

    test: improve unit tests for application state

--- src/core/application_state_json/__init__test.py
+++ src/core/application_state_json/__init__test.py
@@ -1,23 +1,25 @@
 import json
 
-import pytest
-
 from src.core.application_state_json import ApplicationStateJson
 
+test_object_key = "test-object"
+test_object_value = "test-value"
 
-def test(tmp_path):
-    with pytest.raises(RuntimeError):
-        ApplicationStateJson()
+
+def test_clean_state(tmp_path):
     state_save_file_path = tmp_path / "state.json"
     ApplicationStateJson.init(state_save_file_path.__str__())
-    with pytest.raises(RuntimeError):
-        ApplicationStateJson.init(state_save_file_path.__str__())
     assert state_save_file_path.exists()
     assert state_save_file_path.read_text() == ""
-    new_object_key = "test-object"
-    new_object_value = "test-value"
-    ApplicationStateJson.set(new_object_key, new_object_value)
-    assert ApplicationStateJson.get(new_object_key, None) == new_object_value
+    ApplicationStateJson.set(test_object_key, test_object_value)
+    assert ApplicationStateJson.get(test_object_key, None) == test_object_value
     assert state_save_file_path.read_text() == json.dumps(
-        {new_object_key: new_object_value}
+        {test_object_key: test_object_value}
     )
+
+
+def test_existing_state(tmp_path):
+    state_save_file_path = tmp_path / "state.json"
+    state_save_file_path.write_text(json.dumps({test_object_key: test_object_value}))
+    ApplicationStateJson.init(state_save_file_path.__str__())
+    assert ApplicationStateJson.get(test_object_key, None) == test_object_value
