cf3843f2c57aa3794306e48890b32ed25487aeb8
Alexander Ivanov <76620843+ark-key@users.noreply.github.com>
2022-05-27 01:24:28 +0300

    test: refactor tests for application state

--- src/core/application_state_json/__init__test.py
+++ src/core/application_state_json/__init__test.py
@@ -8,13 +8,27 @@ test_object_key = "test-object"
 test_object_value = "test-value"
 
 
-def test_clean_state(tmp_path):
+def test_init_semantics(tmp_path):
+    with pytest.raises(RuntimeError):
+        ApplicationStateJson()
     state_save_file_path = tmp_path / "state.json"
     ApplicationStateJson.init(state_save_file_path.__str__())
     with pytest.raises(RuntimeError):
         ApplicationStateJson.init(state_save_file_path.__str__())
+
+
+def test_clean_state(tmp_path):
+    state_save_file_path = tmp_path / "state.json"
+    assert state_save_file_path.exists() is False
+    ApplicationStateJson.init(state_save_file_path.__str__())
     assert state_save_file_path.exists()
     assert state_save_file_path.read_text() == ""
+    assert ApplicationStateJson.get(test_object_key, None) is None
+
+
+def test_state_set(tmp_path):
+    state_save_file_path = tmp_path / "state.json"
+    ApplicationStateJson.init(state_save_file_path.__str__())
     ApplicationStateJson.set(test_object_key, test_object_value)
     assert ApplicationStateJson.get(test_object_key, None) == test_object_value
     assert state_save_file_path.read_text() == json.dumps(
