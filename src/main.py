d4b6f939931cd5b1559db65c169409ecd8346fdf
Alexander Ivanov <76620843+ark-key@users.noreply.github.com>
2022-05-27 20:46:37 +0300

    refactor: refactor main.py, move backup-witch run logic to run package

--- src/main.py
+++ src/main.py
@@ -1,9 +1,7 @@
 import asyncio
 import logging
-import subprocess
 
-from src.core.application_state_json import ApplicationStateJson
-from src.core.backup_service import BackupService
+from src.run import run
 
 try:
     from config import CONFIG
@@ -17,26 +15,5 @@ logging.basicConfig(
 )
 
 
-async def main():
-    try:
-        ApplicationStateJson.init(CONFIG.STATE_FILE)
-        backup_service = BackupService(
-            application_state=ApplicationStateJson, config=CONFIG
-        )
-        await backup_service.run()
-    except BaseException as e:
-        stderr = ""
-        if type(e) == subprocess.CalledProcessError:
-            e: subprocess.CalledProcessError
-            stderr = f"stderr:\n{e.stderr}\n"
-        logging.critical(f"repr(e):\n{repr(e)}\n{stderr}---")
-        subprocess.run(
-            f'notify-send "backup_witch" "Exception Occurred\nCheck log -> {CONFIG.PYTHON_LOG_FILE}" -u critical',
-            shell=True,
-            check=False,
-        )
-        raise e
-
-
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(run(config=CONFIG))
