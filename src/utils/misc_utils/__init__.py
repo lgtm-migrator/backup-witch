e8b1bbe6ab34a4f789c9f876209d794dcc3bf105
Alexander Ivanov <76620843+ark-key@users.noreply.github.com>
2022-05-29 23:10:34 +0300

    refactor: refactor rclone_log_contains_not_ignored_errors
    Check for unparseable error in rclone log, treat it as not ignored error and return True

--- src/utils/misc_utils/__init__.py
+++ src/utils/misc_utils/__init__.py
@@ -6,8 +6,10 @@ from typing import Callable, TextIO
 def rclone_log_contains_not_ignored_errors(
     file: TextIO, checks: list[Callable[[str, TextIO], bool]]
 ) -> bool:
+    log_file_contains_unparseable_error = True
     for line in file:
         if "ERROR" in line and "Can't retry any of the errors" not in line:
+            log_file_contains_unparseable_error = False
             if all(check(line, file) for check in checks):
                 return True
-    return False
+    return log_file_contains_unparseable_error
