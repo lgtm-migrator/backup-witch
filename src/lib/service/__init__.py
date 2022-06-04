e68e6cced4a6b632bcdfa74a68c258b30cc510d6
Alexander Ivanov <76620843+ark-key@users.noreply.github.com>
2022-06-04 15:05:30 +0300

    refactor: drop asyncio, use time.sleep() instead
    We don't need asynchronous model here, as the only service we run is BackupService

--- src/lib/service/__init__.py
+++ src/lib/service/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-import asyncio
+import time
 from abc import ABC, abstractmethod
 
 from src.lib.application_state import ApplicationState
@@ -17,21 +17,24 @@ class Service(ABC):
     ):
         self._run_interval = run_interval
         self._state = ServiceState(application_state, state_key_prefix)
-        self._lock = asyncio.Lock()
+        self._running = False
 
-    async def run(self):
-        if self._lock.locked():
+    def run(self):
+        if self._running:
             raise RuntimeError(f"{self.__class__} is already running")
 
-        async with self._lock:
+        try:
+            self._running = True
             while True:
                 interval_delta = self._get_interval_delta()
                 if interval_delta <= 0:
-                    await self._body()
+                    self._body()
                     self._state.set(key="last_run_end_time_stamp", value=time_stamp())
-                    await asyncio.sleep(self._run_interval)
+                    time.sleep(self._run_interval)
                 else:
-                    await asyncio.sleep(interval_delta)
+                    time.sleep(interval_delta)
+        finally:
+            self._running = False
 
     def _get_interval_delta(self) -> int:
         return self._run_interval - seconds_passed_from_time_stamp_till_now(
@@ -39,5 +42,5 @@ class Service(ABC):
         )
 
     @abstractmethod
-    async def _body(self):
+    def _body(self):
         pass
